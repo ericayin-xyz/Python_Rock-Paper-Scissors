1a3c7833e4f72dff0b4bd588a9e4a7debfac4358
luyueyin <12898@coderacademy.edu.au>
2022-07-10 00:52:08 +1000

    mian feature finished

--- my_module.py
+++ my_module.py
@@ -2,21 +2,21 @@ import random
 
 scissors = '''
                   ._____
-            _____/    ___)_____.
-                          ______) 
-                         _______)
-            ____    (_____)
-                ` __(____)
+             ____/    ___)______.
+                          _______) 
+                         ________)
+             ___     (_____)
+                ` ___(____)
 
 '''
 
 rock = '''
                 ._____
             ___/    ___)_.
-                   (______) 
-                   (______)
-            __     (_____)
-               ` __(____)
+                   (_______) 
+                   (_______)
+            __     (______)
+               ` __(_____)
    
         '''
 
@@ -34,26 +34,33 @@ images = [rock, paper, scissors]
 
 print("Welcome come to the Rock Paper Scissors!\n")
 user_choice = int(input("1. Rock\n2. Paper\n3. Scissors\n\nWhat do you choose?\n"))
-
-print('''                                             -----
-                                            | YOU |
-                                             -----''')
-print(images[user_choice - 1])
-print('''
-|-------------------------------------------------|
-/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
-|—————————————————————————————————————————————————|
-''')
-computer_choice = random.randint(0,2)
-print(images[computer_choice])
-
-if user_choice > 3 or user_choice - 1 < 0:
+if user_choice > 3 or user_choice < 1:
     print("Ivalid Input: You Lose!")
-elif user_choice - 1 == computer_choice:
-    print("It's a Deaw")
-elif user_choice - 1 > computer_choice:
-    print("You Win!")
-elif user_choice - 1 == 3 and computer_choice == 1:
-    print("You Lose!")
-elif user_choice - 1 == 1 and computer_choice == 3:
-    print("You Win!")
+else:
+    print('''                                             -----
+                                                | YOU |
+                                                -----''')
+    print(images[user_choice - 1])
+    print('''
+    |-------------------------------------------------|
+    /\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
+    |—————————————————————————————————————————————————|
+    ''')
+    computer_choice = random.randint(1,3)
+    print(images[computer_choice - 1])
+
+
+    if user_choice == 3 and computer_choice == 1:
+        print("You Lose!")
+    elif user_choice == 1 and computer_choice == 3:
+        print("You Win!")
+    elif user_choice == computer_choice:
+        print("It's a Deaw")
+    elif user_choice > computer_choice:
+        print("You Win!")
+    elif user_choice < computer_choice:
+        print("You Lose!")
+
+
+# else:
+#     print("Ivalid Input: You Lose!")
