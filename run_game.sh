170f590d0bd26a6d6df1f0e364b3d924041dbfc3
luyueyin <12898@coderacademy.edu.au>
2022-07-16 16:10:23 +1000

    bash script completed

--- run_game.sh
+++ run_game.sh
@@ -4,10 +4,10 @@ if ! [ -x "$(command -v python3)" ]; then
   echo 'Error: python3 is not installed.' >&2
   exit 1
 fi
-python -m ensurepip --upgrade
-pip install -r requirements.txt
+python3 -m ensurepip --upgrade
+pip3 install -r requirements.txt
 
-echo 'You can run <./run_game.sh help>, if you need further help.'
+echo 'Type "./run_game.sh help" for more information.'
 
 if [ $# -eq 1 ] && [ $1 == "help" ]; then
     cat help.txt
